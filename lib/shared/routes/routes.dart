2f4bacc91b64fdad9803577765f4024469ed8285
carloscasanovad <carlos.casanova@warren.com.br>
2022-09-21 23:42:27 -0300

    refactor/EV-839/Switching routes for onGenerate routes

--- lib/shared/routes/routes.dart
+++ lib/shared/routes/routes.dart
@@ -4,12 +4,6 @@ import 'package:flutter/material.dart';
 import '../../details/views/details_page.dart';
 import '../../wallet/views/wallet_page.dart';
 
-// Map<String, Widget Function(BuildContext)> routes = {
-//   WalletPage.route: (context) => const WalletPage(),
-//   // TransactionsPage.route: (context) => const TransactionsPage(),
-//   DetailsPage.route: (context) => DetailsPage(),
-// };
-
 Route<dynamic>? onGenerateRoute(settings) {
   if (settings.name == WalletPage.route) {
     return PageRouteBuilder(
